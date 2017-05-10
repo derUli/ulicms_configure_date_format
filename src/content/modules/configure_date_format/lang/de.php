4aba7f200664c05dabee68594f80821fec3206b9
Ulrich Schmidt <u.schmidt@velian.de>
2017-05-10 14:28:13 +0200

    sprachdatei

---
+++ src/content/modules/configure_date_format/lang/de.php
@@ -0,0 +1,3 @@
+<?php
+define ( "TRANSLATION_CONFIGURE_DATE_FORMAT", "Datumsformat einstellen" );
+define ( "TRANSLATION_DATE_FORMAT", "Datumsformat" );
---
+++ src/content/modules/configure_date_format/lang/en.php
@@ -0,0 +1,3 @@
+<?php
+define ( "TRANSLATION_CONFIGURE_DATE_FORMAT", "Configure date format" );
+define ( "TRANSLATION_DATE_FORMAT", "Date format" );
